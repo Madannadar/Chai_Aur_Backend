62f9cd02473ab61d52c31eb30350214711581648
Madannadar <154582270+Madannadar@users.noreply.github.com>
2025-01-20 16:57:59 +0530

    wrote the controller for login and logout added cookies made it cookies to undefined

--- Major_project/src/controllers/user.controller.js
+++ Major_project/src/controllers/user.controller.js
@@ -4,6 +4,22 @@ import { User } from "../models/user.models.js" // can input { } if the export i
 import { uploadOnCloudinary } from '../utils/cloudinary.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 
+const generateAccessAndRefereshTokens = async(userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refereshToken = user.generateRefreshToken()
+
+        user.refreshToken = refereshToken
+        await user.save({validateBeforeSave: false}) // while using mongodb method all the checking will kickin from the model so we use {validateBeforeSave: false}
+
+        return {accessToken, refereshToken}
+
+    } catch (error) {
+        throw new ApiError(500, 'something went wrong while generating referesh and access token')
+    }
+}
+
 const registerUser = asyncHandler( async(req, res) => {
     // logic building for register
 
@@ -87,4 +103,79 @@ const registerUser = asyncHandler( async(req, res) => {
     )
 })
 
-export {registerUser}
+const loginUser = asyncHandler(async (req, res) => {
+    // req body => data
+    // username / email 
+    // find the user
+    // password check
+    // access and referesh token generate
+    // send this token by cookie
+
+    const {email, username, password} = req.body
+
+    if( !username || !email){
+        throw new ApiError(400, "username or email is required")
+    }
+
+    const user = await User.findOne({ $or: [{username}, {email}] }) // User is a user made by mongodb to use findOne we use User 
+    if(!user) {
+        throw new ApiError(404, "user does not exist")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if(!isPasswordValid) {
+        throw new ApiError(401, "password incorrect")
+    }
+
+    const {accessToken, refereshToken} = await generateAccessAndRefereshTokens(user._id)
+
+    const loggedInUser = await User.findById(user._id)
+    select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    } // now only the cookies can be modified by server only
+
+    return res.status(200)
+    .cookie("accessToken",accessToken, options)
+    .cookie("refreshToken",refereshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refereshToken
+            },
+            "user logged in successfully"
+        )
+    )
+})
+
+const logoutUser = asyncHandler(async(req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200,{},"User Logged out"))
+})
+
+export {
+    registerUser,
+    loginUser,
+    logoutUser
+}
