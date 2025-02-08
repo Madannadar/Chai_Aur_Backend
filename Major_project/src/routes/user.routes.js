67e2f3711559931f6ea05987d172b1f794021b81
Madannadar <154582270+Madannadar@users.noreply.github.com>
2025-02-08 17:15:46 +0530

    add new user routes for password change, current user retrieval, profile updates, avatar and cover image uploads, and watch history

--- Major_project/src/routes/user.routes.js
+++ Major_project/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { loginUser, logoutUser, refereshAccessToken, registerUser } from '../controllers/user.controller.js';
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refereshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -24,5 +24,11 @@ router.route('/login').post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route('/refresh-token').post(refereshAccessToken)
-
+router.route('/change-password').post(verifyJWT, changeCurrentPassword);
+router.route('/current-user').get(verifyJWT, getCurrentUser)
+router.route('/update-profile').patch(verifyJWT, updateAccountDetails) // patch matlab only the one changeing will be updated not all 
+router.route('/avatar').patch(verifyJWT, upload.single('avatar'), updateUserAvatar)
+router.route('/cover-image').patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage)
+router.route('/c/:username').get(verifyJWT, getUserChannelProfile) // this : is used in router because we are taking data from params
+router.route('/history').get(verifyJWT, getWatchHistory)
 export default router 
